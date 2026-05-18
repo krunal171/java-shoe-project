b6270581c43ee7ce080afe85478a75288eb8310f
krunal171 <ranakrunal176@gmail.com>
2026-05-18 17:05:03 +0530

    18/5-26 updated

--- Shoe_Store/src/main/java/com/mycompany/shoe_store/Orders.java
+++ Shoe_Store/src/main/java/com/mycompany/shoe_store/Orders.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Date;
-
+// =====================================================
 @Entity
 @Table(name = "orders")
 public class Orders implements Serializable {
